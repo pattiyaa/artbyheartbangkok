5f86106ea8b029764560a5fa1dd039e398d79405
pattiyaa <pattiyaa@gmail.com>
2016-11-05 13:39:11 +0700

    add precompile for ckeditor

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module Artbyheartbangkok
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+	config.assets.precompile += Ckeditor.assets
+	config.assets.precompile += %w(ckeditor/*)
   end
 end
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-config.assets.precompile += Ckeditor.assets
-config.assets.precompile += %w(ckeditor/*)
