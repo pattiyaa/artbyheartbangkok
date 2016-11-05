f14de9ceb97666f83c46f8103970450c730cbe34
pattiyaa <pattiyaa@gmail.com>
2016-11-05 19:20:40 +0700

    load assert/admin/article.css

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,11 @@ module Artbyheartbangkok
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+	config.assets.enabled = true
+	
 	config.assets.precompile += Ckeditor.assets
-	config.assets.precompile += %w(ckeditor/*)
+	# config.assets.precompile += %W(#{config.root}/app/models/ckeditor)
+	# config.assets.precompile += %W(#{config.root}/app/assert/ckeditor)
+	
   end
 end
